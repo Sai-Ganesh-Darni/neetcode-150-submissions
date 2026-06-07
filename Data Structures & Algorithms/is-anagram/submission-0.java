abae3c6edc9c3f94dc27a8667a9297903d0ebd74
Sai Ganesh Darni <saiganeshdarni@gmail.com>
2026-06-07 17:17:16 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+        int len = s.length();
+        int freq[] = new int[26];
+        for(char ch:s.toCharArray()){
+            freq[ch-'a']++;
+        }
+        for(char ch:t.toCharArray()){
+            freq[ch-'a']--;
+        }
+        for(int i:freq){
+            if(i!=0)
+                return false;
+        }
+        return true;
+    }
+}
